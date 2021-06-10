7dee8bbacd9604ffc2415d03605fd23bfb55cd6f
Duane Rinehart <drinehar@sdccd.edu>
2021-06-09 19:31:21 -0700

    modified schema: adjusted close may be null until calculated

--- db_schema.sql
+++ db_schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS sp500 (
 	`date` datetime NOT NULL PRIMARY KEY,
 	`open` DECIMAL NOT NULL,
 	`close` DECIMAL NOT NULL,
-	`adj_close` DECIMAL NOT NULL,
+	`adj_close` DECIMAL,
 	`high` DECIMAL NOT NULL,
 	`low` DECIMAL NOT NULL,
 	`volume` INT NOT NULL
